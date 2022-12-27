fa73e7c8d17736395a51e3f98002c8aa97da9f99
mitu1403 <mmitu163069@bscse.uiu.ac.bd>
2022-12-27 14:01:29 +0600

    First One-way Data Flow

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-
-import './App.css';
-import Button from './components/Button';
+import { useState } from "react";
+import "./App.css";
+import Button from "./components/Button";
+import Display from "./components/Display";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const handleClick = () => setCounter(counter + 2);
   return (
-    <div>
-      <Button/>      
+    <div style={{ textAlign: "center", margin: "24% 0px" }}>
+      <Button onClickFunction={handleClick} />
+      <Display message={counter} />
     </div>
   );
 }
 
 export default App;
+
+
+
+// Parent Component can flow their data & behavior down to children components:
+
+//   parent   -> App.js
+//   children -> Button.js, Display.js
+
+//   data -> counter
+//   behavior -> handleClick()
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,26 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
-function logRandom() {
-  console.log(Math.random());
-}
-function Button() {
-  const [counter, setCounter] = useState(1);
-  // function handleClick() {
-  //   setCounter(counter + 1);
-  // }
-  // can use arrow function instead of using like this. 
-  // But its good to make another function.
-
+function Button(props) {
   return (
-    <button onClick={() => setCounter(counter * 2)}>
-      Count - {counter}
+    <button onClick={props.onClickFunction}>
+      Increment +2
     </button>
   );
 }
 
 export default Button;
-
-// useState() results:
-//   - state object (getter)
-//   - updater function (setter)
-// const [ currentStateValue, functiopnToSetNewStateValue ] = useState(initialValue)
